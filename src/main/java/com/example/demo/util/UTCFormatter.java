92b46d12f98cc6a76eaafa4af5e0811a135c8101
Alisher Khairullin <alisherkhairullin463@gmail.com>
2024-12-03 17:37:21 +0500

    rework: utc formatter

--- src/main/java/com/example/demo/util/UTCFormatter.java
+++ src/main/java/com/example/demo/util/UTCFormatter.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 
 @Component
 public class UTCFormatter {
     public LocalDateTime convertUTCToUTCPlus5(LocalDateTime dateTime) {
         return dateTime.atOffset(ZoneOffset.UTC)
                 .withOffsetSameInstant(ZoneOffset.ofHours(5))
-                .toLocalDateTime();
+                .toLocalDateTime()
+                .withNano(0)
+                .truncatedTo(ChronoUnit.MINUTES);
     }
 }
